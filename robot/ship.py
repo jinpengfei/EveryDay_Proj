8ff5b65bc5abff97a428c1b28710648412af6c2c
jinpengfei <jinpengfei@local.com>
2020-08-22 15:56:30 +0800

    驾驶飞船

---
+++ robot/ship.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: iso-8859-15 -*-
+import pygame
+
+class Ship():
+	def __init__(self, ai_settings,screen):
+		self.screen = screen
+		self.ai_settings = ai_settings
+		
+		self.image = pygame.image.load('images/ship.png')
+		self.rect = self.image.get_rect()
+		self.screen_rect = screen.get_rect()
+		self.rect_centerx = self.screen_rect.centerx
+		self.rect.bottom = self.screen_rect.bottom
+		self.moving_right = False
+		self.moving_left = False
+		self.center = float(self.rect.centerx)
+	def update(self):
+		"""根据移动标志调整飞船的位置"""
+		if self.moving_right:
+			self.center += self.ai_settings.ship_speed_factor
+		if self.moving_left:
+			self.center -= self.ai_settings.ship_speed_factor
+		self.rect.centerx = self.center
+	def blitme(self):
+		self.screen.blit(self.image, self.rect)
